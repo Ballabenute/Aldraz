1b1a9fde1fb9d9164efde42b31185361b61b2e16
Ballabenute <VITOR.BALLABENUTE@GMAIL.COM>
2024-09-17 22:11:23 -0300

    Inicio do Projeot

---
+++ API/Aldraz/Models/Domain/Raça.cs
@@ -0,0 +1,9 @@
+﻿namespace Aldraz.Models.Domain
+{
+    public class Raça
+    {
+        public  Guid Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
